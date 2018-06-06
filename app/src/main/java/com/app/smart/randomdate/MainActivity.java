1bf23bcf9805eaffa6f355458cafca0469e85261
Mohamed <mohfoad@gmail.com>
2018-06-06 14:43:21 +0200

    user entry date added

--- app/src/main/java/com/app/smart/randomdate/MainActivity.java
+++ app/src/main/java/com/app/smart/randomdate/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,13 +22,16 @@ public class MainActivity extends AppCompatActivity {
 TextView dayRandomNumber,monthRandomNumber,yearRandomNumber, trialCount;
 Button generateRandomNumber;
 
-Random r;
+EditText dayEnteredNumber, monthEnteredNumber, yearEnteredNumber;
+
+
+Random random;
 
 int randomDay, randomMonth, randomYear, trialCountNumber;
 
-int max = 30;
+int dayToGet, monthToGet, yearToGet;
 
-int min = 0;
+String dayUser, monthUser, yearUser;
 
 
 
@@ -36,52 +40,82 @@ int min = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        r = new Random();
+        random = new Random();
+        //random day, month and year
+        dayRandomNumber      = findViewById(R.id.random_day_text_view);
+        monthRandomNumber    = findViewById(R.id.random_month_text_view);
+        yearRandomNumber     = findViewById(R.id.random_year_text_view);
 
-        dayRandomNumber= findViewById(R.id.random_day_text_view);
+        //how many users hit the luck point to get his entered date
+        trialCount           = findViewById(R.id.trialCount);
 
-        monthRandomNumber= findViewById(R.id.random_month_text_view);
-
-        yearRandomNumber= findViewById(R.id.random_year_text_view);
-
-        trialCount = findViewById(R.id.trialCount);
+        //Entered user day, month and year
+        dayEnteredNumber     = findViewById(R.id.entered_day_text_view);
+        monthEnteredNumber   = findViewById(R.id.entered_month_text_view);
+        yearEnteredNumber    = findViewById(R.id.entered_year_text_view);
 
         generateRandomNumber = findViewById(R.id.generate_random_number);
 
-        //final int result = ((int)(Math.random() * 10)) + 1;
-
+        //initial value for counter
         trialCountNumber = 0;
-
-        //Log.d("TAG", getString(random));
 
         generateRandomNumber.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                randomDay =r.nextInt(30 ) + 1;
+                //user data get from another method
+               // getUserDate();
 
-                randomMonth = r.nextInt(12)+1;
+                dayUser     = dayEnteredNumber.getText().toString();
+                monthUser   = monthEnteredNumber.getText().toString();
+                yearUser    = yearEnteredNumber.getText().toString();
 
-                randomYear = r.nextInt(118) +1900 ;
 
-            if (randomMonth == 1 && randomDay==30){
-                randomDay =r.nextInt(27 ) + 1;
-            }
 
-            if (randomMonth == 1 && randomDay==29){
-                randomDay =r.nextInt(25 ) + 1;
-            }
 
-            if (randomMonth == 1 && randomDay==28){
-                randomDay =r.nextInt(26 ) + 1;
-            }
 
-            trialCountNumber++;
-            dayRandomNumber.setText(String.valueOf(randomDay));
-            monthRandomNumber.setText(String.valueOf(randomMonth));
-            yearRandomNumber.setText(String.valueOf(randomYear));
-            trialCount.setText(String.valueOf(trialCountNumber));
+                if (!dayUser.equals("") &&
+                    !monthUser.equals("") &&
+                    !yearUser.equals(""))
+                {
+                    dayToGet = Integer.parseInt(dayUser);
+                    monthToGet = Integer.parseInt(monthUser);
+                    yearToGet = Integer.parseInt(yearUser);
+
+                    randomDay = random.nextInt(dayToGet) +1;
+                    randomMonth = random.nextInt(monthToGet) +1;
+                    randomYear = random.nextInt(yearToGet) +1;
+
+                    if (randomDay == dayToGet &&
+                            randomMonth == monthToGet &&
+                            randomYear == yearToGet) {
+
+                        trialCount.setTextColor(getResources().getColor(R.color.colorAccent));
+                        Toast.makeText(MainActivity.this, " correct", Toast.LENGTH_LONG).show();
+                    }
+                    /*
+                    if (randomMonth == 1 && randomDay==30){
+                        randomDay =random.nextInt(27 ) + 1;
+                    }
+
+                    if (randomMonth == 1 && randomDay==29){
+                        randomDay =random.nextInt(25 ) + 1;
+                    }
+
+                    if (randomMonth == 1 && randomDay==28){
+                        randomDay =random.nextInt(26 ) + 1;
+                    }
+                    */
+                    trialCountNumber++;
+
+                    dayRandomNumber.setText(String.valueOf(randomDay));
+                    monthRandomNumber.setText(String.valueOf(randomMonth));
+                    yearRandomNumber.setText(String.valueOf(randomYear));
+                    trialCount.setText(String.valueOf(trialCountNumber));
+                }else{
+                    Toast.makeText(MainActivity.this," Please set your date data",Toast.LENGTH_LONG).show();
+                }
             }
         });
 
