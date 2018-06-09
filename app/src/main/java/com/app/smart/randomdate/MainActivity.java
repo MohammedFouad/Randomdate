acfb6fe33a39a35a84fd98c343e66288390f4a3d
Mohamed <mohfoad@gmail.com>
2018-06-09 12:24:36 +0200

    Date picker added and reset counter button

--- app/src/main/java/com/app/smart/randomdate/MainActivity.java
+++ app/src/main/java/com/app/smart/randomdate/MainActivity.java
@@ -1,5 +1,7 @@
 package com.app.smart.randomdate;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.nfc.Tag;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +28,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.Random;
@@ -37,12 +41,20 @@ public class MainActivity extends AppCompatActivity {
 
     TextView dayRandomNumber, monthRandomNumber, yearRandomNumber, trialCount;
     Button generateRandomNumber;
-    EditText dayEnteredNumber, monthEnteredNumber, yearEnteredNumber;
+    TextView dayEnteredNumber, monthEnteredNumber, yearEnteredNumber;
     CheckBox easyCheckBox, mediumCheckBox, hardCheckBox;
 
     int randomDay, randomMonth, randomYear, trialCountNumber;
     int dayToGet, monthToGet, yearToGet;
     String dayUser, monthUser, yearUser;
+
+    Button resetButton;
+
+    boolean counterReset;
+
+    private int year, month, day;
+
+    int point;
 
     Random random;
 
@@ -76,6 +88,9 @@ public class MainActivity extends AppCompatActivity {
         mediumCheckBox = findViewById(R.id.mediumCheckBox);
         hardCheckBox = findViewById(R.id.hardCheckBox);
 
+        resetButton = findViewById(R.id.resetCount);
+        resetButton.setVisibility(resetButton.GONE);
+
         //initial value for counter
         trialCountNumber = 0;
         generateRandomNumber.setOnClickListener(new View.OnClickListener() {
@@ -87,7 +102,12 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
+                if(counterReset){
+                    return;
+                }
+
                 getEntryDate();
+
                 if (easyCheckBox.isChecked()) {
 
                     randomDay = random.nextInt(30) + 1;
@@ -96,12 +116,15 @@ public class MainActivity extends AppCompatActivity {
 
                     if (!randomEqualEntry()) {
 
-
                         YoYo.with(Techniques.Tada)
                                 .duration(700)
                                 .repeat(10)
                                 .playOn(findViewById(R.id.trialCount));
                         trialCount.setTextColor(getResources().getColor(R.color.colorTrialCount));
+                       counterReset = true;
+                        resetButton.setVisibility(resetButton.VISIBLE);
+                        generateRandomNumber.setVisibility(View.GONE);
+                        point++;
 
 
                     }
@@ -119,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
                     if (!randomEqualEntry()) {
                         Toast.makeText(MainActivity.this, " well done ", Toast.LENGTH_LONG).show();
                         trialCountNumber = 0;
+                        point = point + 10;
                     }
                     displayRandomDate();
                     trialCountNumber++;
@@ -132,6 +156,7 @@ public class MainActivity extends AppCompatActivity {
                     if (!randomEqualEntry()) {
                         Toast.makeText(MainActivity.this, " well done ", Toast.LENGTH_LONG).show();
                         trialCountNumber = 0;
+                        point = point + 100;
                     }
                     displayRandomDate();
                     trialCountNumber++;
@@ -139,8 +164,89 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        final Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+
+        showDate(year, month + 1, day);
+
     }
 
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2 + 1, arg3);
+                }
+            };
+
+
+    private void showDate(int year1, int month1, int day1) {
+
+        dayEnteredNumber.setText(String.valueOf(day1));
+        monthEnteredNumber.setText(String.valueOf(month1));
+        yearEnteredNumber.setText(String.valueOf(year1));
+    }
+
+    // set method to countdown timer when user guess success
+
+    // set method to reset countdown
+
+    // set method to show points
+
+    public void resetCounter(View view){
+        counterReset = false;
+        trialCountNumber = 0;
+
+        resetButton.setVisibility(resetButton.GONE);
+        generateRandomNumber.setVisibility(View.VISIBLE);
+        trialCount.setTextColor(getResources().getColor(R.color.colorTrialCountContinue));
+        displayRandomDate();
+
+
+    }
+/*
+    private boolean delayTimeFinished(){
+        final boolean[] isTimeFinished = {true};
+    new CountDownTimer(30000, 1000) {
+
+        public void onTick(long millisUntilFinished) {
+            isTimeFinished[0] = false;
+        }
+        public void onFinish() {
+            isTimeFinished[0] = false;
+        }
+
+    }.start();
+
+    return isTimeFinished[0];
+    }
+*/
     private boolean validateEntryData() {
         boolean result = true;
         if (TextUtils.isEmpty(dayEnteredNumber.getText().toString())) {
